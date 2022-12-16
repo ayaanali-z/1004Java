88a747d453048b0ee633185fa0e7728f3a1779e6
Ayaan Ali <ayaanali5204@outlook.com>
2022-12-15 23:06:07 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -14,67 +14,58 @@ public class Player {
 	
 		
 	private ArrayList<Card> hand; 
-
 	private double bankroll;
-
-    private double bet;
+	private double bet;
 		
-	public Player(){	
+	public Player(){
+		
         bankroll = 50.0; // tokens 
-
         bet = 0.0; 
-
         hand = new ArrayList<Card>(); // creates hand
  	}
 
 	public void addCard(Card c){
             hand.add(c); // adds card to hand 
 	}
-
-    public void addCard(Card c, int index) { 
+	
+	public void addCard(Card c, int index) { 
             hand.add(index, c); /* overloaded addCard method to 
             assist in preventing players from repeatedly exchanging 
             the same card in the Game class */
-    }
+	}
 
 	public void removeCard(Card c){
             hand.remove(c); // removes card from hand 
         }
 
-    public ArrayList<Card> getHand() { 
-        return hand; // gives hand 
-    }
+    	public ArrayList<Card> getHand() { 
+        	return hand; // gives hand 
+    	}
 
-    public Card getCard(int i) { 
-        return hand.get(i); // gives specific card from hand 
-    }
+    	public Card getCard(int i) { 
+        	return hand.get(i); // gives specific card from hand 
+    	}
 		
-    public void bets(double amt){ 
-        if (amt <= bankroll && amt < 6 && amt > 0) { // input validatino 
-            
-            bet = amt; 
-            
-             bankroll -= bet; 
-        }
-            
-        else { 
+    	public void bets(double amt){ 
+        	if (amt <= bankroll && amt < 6 && amt > 0) { // input validation
+            	bet = amt;
+             	bankroll -= bet; 
+        	}
+        	else { 
                 System.out.println("You can't bet more than your bankroll."); 
-        } 
-    }
+        	} 
+    	}
 
-    public void winnings(double odds){
+    	public void winnings(double odds){
 		
-        double winnings = (bet * odds); // increases winnings with higher bets
-        
-        bankroll+=winnings; 
-		
-        System.out.println("YOU WON " + winnings + " TOKENS!"); 
-    }
+        	double winnings = (bet * odds); // increases winnings with higher bets
+        	bankroll+=winnings; 
+        	System.out.println("YOU WON " + winnings + " TOKENS!"); 
+    	}
 
-    public double getBankroll(){
-            return bankroll; // returns bankroll 
-    }
-		
+    	public double getBankroll(){
+            	return bankroll; // returns bankroll 
+    	}	
 }
 
 
