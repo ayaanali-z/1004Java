6ab48a71e61839000041790c4e8ae8ce7a61bbe4
Ayaan Ali <ayaanali5204@outlook.com>
2022-12-15 23:02:58 -0500

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -16,36 +16,30 @@ public class Deck {
 	
 	public Deck(){ 
 		int cardNumber = 0; 
-
 		top = 0; 
-
 		cards = new Card[52]; // array of 52 cards 
-			for(int suit = 1; suit < 5; suit++) { // for each suit 
+			
+		for(int suit = 1; suit < 5; suit++) { // for each suit 
 
-				for (int rank = 1; rank < 14; rank++) { // creates a card from 1-13 
-					cards[cardNumber] = new Card(suit, rank);  
+			for (int rank = 1; rank < 14; rank++) { // creates a card from 1-13 
+				cards[cardNumber] = new Card(suit, rank);  
 
-					cardNumber++; 
-				}
+				cardNumber++; 
 			}
-
+		}
 	}
 	
 	public void shuffle(){ // shuffles the deck 1k times 
 		while(shuffleNumber < 1000) { 
 			int a = (int)(Math.random()*52); // random number from 0 to 52 
-
 			int b = (int)(Math.random()*52); 
 
 			Card temp = cards[a]; // temp card variable 
-
 			cards[a] = cards[b]; // card a is assigned to b 
-
 			cards[b] = temp; // b takes card a's spot 
 
 			shuffleNumber++; 
 		}
-
 	}
 	
 	public Card deal(){
@@ -54,13 +48,7 @@ public class Deck {
 
 	public void returnDeck() { 
 		for(int i = 0; i < cards.length; i++) { 
-
 			System.out.println(cards[i]); // prints every card in the deck
 		}
-
 	}
-	
-
-
-
 }
